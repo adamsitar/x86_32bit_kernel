7f2877f76c270420cd249836cba51454b5351f07
adamsitar <adam.sitar1@gmail.com>
2025-07-31 22:37:39 +0200

    improved folder structure, fixed build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 nasm -felf32 boot.asm -o bin/boot.o
-nasm -felf32 kernel.asm -o bin/kernel.o
+i686-elf-gcc -c kernel.c -o bin/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 ld -m elf_i386 -T linker.ld -o bin/os.bin bin/boot.o bin/kernel.o
-qemu-system-i386 -kernel bin/os.bin
-grub-mkrescue -o myos.iso isodir
-qemu-system-i386 -cdrom myos.iso
+
+mkdir -p bin/isodir/boot/grub
+cp bin/os.bin bin/isodir/boot/os.bin
+cp grub.cfg bin/isodir/boot/grub/grub.cfg
+grub-mkrescue -o bin/os.iso bin/isodir
+
+qemu-system-i386 -cdrom bin/os.iso
